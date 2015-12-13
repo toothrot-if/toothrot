fb4b040533d2329ee84ba0a23e9e60a97d426e9b
Jonathan Steinbeck <jonathan@steinbeck.in>
2015-12-14 00:40:59 +0100

    Added bin file.

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
-/* global require, process */
+/* global require */
 
-var fs = require("fs");
-var pack = require("./src/packer").pack;
-var parse = require("./src/parser").parse;
-var build = require("./src/builder").build;
-var init = require("./src/initializer").init;
-var args = process.argv;
-var command = args[2];
-var path = args[3];
-var outputDir = args[4];
-
-if (command === "build") {
-    build(path, outputDir);
-}
-else if (command === "pack") {
-    console.log(pack(path));
-}
-else if (command === "parse") {
-    console.log(JSON.stringify(parse("" + fs.readFileSync(path)), null, 4));
-}
-else if (command === "init") {
-    init(path || process.cwd());
-}
-else {
-    console.log("Usage: build [inputDir] [outputDir]");
-    console.log("Usage: pack [inputDir]");
-    console.log("Usage: parse [storyFile]");
-}
+module.exports = {
+    build: require("./src/builder").build,
+    init: require("./src/initializer").init,
+    pack: require("./src/packer").pack,
+    parse: require("./src/parser").parse
+};
