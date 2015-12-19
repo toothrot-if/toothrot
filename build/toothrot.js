eaa9d60b6386dfb55e3d6e0d4f44554f2754379e
Jonathan Steinbeck <jonathan@steinbeck.in>
2015-12-19 23:05:43 +0100

    Created new build.

--- build/toothrot.js
+++ build/toothrot.js
@@ -1,6 +1,6 @@
 /*
-    Toothrot Engine (v1.2.0-alpha.1512191113)
-    Build time: Sat, 19 Dec 2015 22:03:18 GMT
+    Toothrot Engine (v1.2.0-alpha.1512192304)
+    Build time: Sat, 19 Dec 2015 22:05:30 GMT
 */
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (global){
