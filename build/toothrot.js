76e8c1a83732eabadb912629173b20556183e0e1
Jonathan Steinbeck <jonathan@steinbeck.in>
2016-03-05 15:45:16 +0100

    Changed version to 1.4.1-beta.

--- build/toothrot.js
+++ build/toothrot.js
@@ -1,6 +1,6 @@
 /*
-    Toothrot Engine (v1.4.0-beta.1512211114)
-    Build time: Tue, 22 Dec 2015 14:10:02 GMT
+    Toothrot Engine (v1.4.1-beta.1603051543)
+    Build time: Sat, 05 Mar 2016 14:43:51 GMT
 */
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (global){
