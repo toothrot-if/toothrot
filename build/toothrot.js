90fef23e83b1a5e6d7cf259c4aa5f112ca7a2d3c
Jonathan Steinbeck <jonathan@steinbeck.in>
2016-09-13 23:57:40 +0200

    Move default UIBar to the top

--- build/toothrot.js
+++ build/toothrot.js
@@ -1,6 +1,6 @@
 /*
     Toothrot Engine (v1.5.0)
-    Build time: Mon, 12 Sep 2016 20:23:17 GMT
+    Build time: Tue, 13 Sep 2016 21:57:23 GMT
 */
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (global){
@@ -5460,6 +5460,7 @@ function run (resources, _, opt) {
         currentNode = undefined;
         text.innerHTML = "";
         stack = [];
+        container.setAttribute("data-section", nodes.start.section);
         clearVars();
         emit("clearState");
     }
