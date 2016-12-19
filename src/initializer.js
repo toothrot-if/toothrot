7fd0f89b88f741652fef13a70c30fb40df908311
Jonathan Steinbeck <mail@steinbeck.it>
2016-12-19 01:19:54 +0100

    Set story name to folder name when initializing

--- src/initializer.js
+++ src/initializer.js
@@ -7,10 +7,21 @@ var ncp = require("ncp").ncp;
 
 var resourceDir = path.normalize(__dirname + "/../resources/");
 
+function getFolderName (dirPath) {
+    
+    var parts = path.normalize(dirPath).split(path.sep);
+    
+    parts.pop();
+    
+    return parts.pop();
+}
+
 function init (dir, then) {
     
+    var name = getFolderName(dir) || "My Toothrot Engine Project";
+    
     var project = {
-        name: "My Toothrot Engine Project",
+        name: name,
         version: "0.1.0",
         electron: {
             platform: ["darwin", "linux", "win32"],
@@ -33,10 +44,21 @@ function init (dir, then) {
         }
         
         fs.writeFileSync(path.normalize(dir + "/project.json"), JSON.stringify(project, null, 4));
+        setStoryName();
         
-        console.log("Initialized Toothrot Engine project in " + dir + ".");
+        console.log("Initialized Toothrot Engine project '" + name + "' in " + dir + ".");
         then(null);
     });
+    
+    function setStoryName () {
+        
+        var file = path.normalize(dir + "/resources/story.md");
+        var story = "" + fs.readFileSync(file);
+        
+        story = story.replace(/^(\s*)#:([^\n]*)/g, "$1#: " + name);
+        
+        fs.writeFileSync(file, story);
+    }
 }
 
 module.exports.init = init;
