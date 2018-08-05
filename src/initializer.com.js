40f29680353556601f4a21d0e954cc83c5e302f5
Jonathan Steinbeck <mail@steinbeck.it>
2018-08-05 10:50:47 +0200

    Improve initializer logging

--- src/initializer.com.js
+++ src/initializer.com.js
@@ -1,5 +1,5 @@
 /* global require, __dirname */
-/* eslint-disable camelcase, no-console */
+/* eslint-disable camelcase */
 
 var fs = require("fs");
 var path = require("path");
@@ -9,22 +9,26 @@ var resourceDir = path.normalize(__dirname + "/../resources/");
 
 function create(context) {
     
+    var logger;
+    
     var api = context.createInterface("initializer", {
         getFolderName: getFolderName,
         init: initProject
     });
     
     function init() {
+        logger = context.getInterface("logger", ["log", "error", "info", "success"]);
         context.connectInterface(api);
     }
     
     function destroy() {
+        logger = null;
         context.disconnectInterface(api);
     }
     
     function getFolderName(dirPath) {
         
-        var parts = path.normalize(dirPath).split(path.sep);
+        var parts = path.normalize(dirPath + "/").split(path.sep);
         
         parts.pop();
         
@@ -34,6 +38,8 @@ function create(context) {
     function initProject(dir, then) {
         
         var name = getFolderName(dir) || "My Toothrot Engine Project";
+        
+        logger.info("Initializing new toothrot project `" + name + "` in `" + dir + "`...");
         
         var project = {
             name: name,
@@ -52,23 +58,31 @@ function create(context) {
         dir = path.normalize(dir + "/");
         
         if (!fs.existsSync(dir)) {
+            logger.log("Directory `" + dir + "` doesn't exist; creating...");
             fs.mkdirSync(dir);
         }
+        
+        logger.info("Copying required resources...");
         
         ncp(resourceDir, dir, function (error) {
             
             if (error) {
                 then(error);
-                return console.error(error);
+                return logger.error(error);
             }
+            
+            logger.success("Resources copied.");
+            logger.log("Creating `project.json` file...");
             
             fs.writeFileSync(
                 path.normalize(dir + "/project.json"), JSON.stringify(project, null, 4)
             );
             
+            logger.log("Setting story title to project name...");
+            
             setStoryName();
             
-            console.log("Initialized Toothrot Engine project '" + name + "' in " + dir + ".");
+            logger.success("Initialized Toothrot Engine project '" + name + "' in " + dir + ".");
             then(null);
         });
         
@@ -77,7 +91,7 @@ function create(context) {
             var file = path.normalize(dir + "/resources/story.trot.md");
             var story = "" + fs.readFileSync(file);
             
-            story = story.replace(/^(\s*)#:([^\n]*)/g, "$1#: " + name);
+            story = story.replace(/^(\s*)#([^\n]*)/g, "$1# " + name);
             
             fs.writeFileSync(file, story);
         }
